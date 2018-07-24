fe6ced9abefc3ec9cc87b3aefd44449eafee3208
Alauddin Ahmed <reptoedu.alauddin@gmail.com>
2018-07-24 19:47:03 +0600

    added a simple pie chart with default dataset

--- admin/gl_admindashboard.php
+++ admin/gl_admindashboard.php
@@ -6,5 +6,18 @@
 </head>
 <body>
 	<h1>Bingo</h1>
+	<div class="graphProject" id="app">
+		<div class="graphOptions">
+			<label for="labels">Labels</label>
+			<input type="text" name="labels" id="labels"><br /><br />
+			<label for="datasets">Datasets</label>
+			<input type="text" name="datasets" id="datasets"><br /><br />
+			<label for="datasets">Datasets</label>
+			<input type="text" name="datasets" id="datasets">
+		</div>
+		<div class="graphDiv">
+			<canvas id="barChart"></canvas>
+		</div>
+	</div>
 </body>
 </html>
