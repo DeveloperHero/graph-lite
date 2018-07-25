d309cd830976070eb089f60fdd2ffc259474c880
Alauddin Ahmed <reptoedu.alauddin@gmail.com>
2018-07-25 15:39:32 +0600

    Instantly updating graph according to input field data

--- admin/gl_admindashboard.php
+++ admin/gl_admindashboard.php
@@ -8,12 +8,20 @@
 	<h1>Bingo</h1>
 	<div class="graphProject" id="app">
 		<div class="graphOptions">
-			<label for="labels">Labels</label>
-			<input type="text" name="labels" id="labels"><br /><br />
-			<label for="datasets">Datasets</label>
-			<input type="text" name="datasets" id="datasets"><br /><br />
-			<label for="datasets">Datasets</label>
-			<input type="text" name="datasets" id="datasets">
+			<table class="form-table">
+				<tr>
+					<th scope="row"><label for="labels">Labels</label></th>
+					<td><input type="text" id="labels" v-model="chartlabelString" @keyup="addLabels"></td>
+				</tr>
+				<tr>
+					<th scope="row"><label for="datasets">Datasets</label></th>
+					<td><input type="text" id="datasets" v-model="chartDatasetDataString" @keyup="addDatasetData"></td>
+				</tr>
+				<tr>
+					<th scope="row"><label for="colors">Color</label></th>
+					<td><input type="text" id="colors" v-model="chartDatasetBgColorString" @keyup="addDatasetBgColor"></td>
+				</tr>
+			</table>
 		</div>
 		<div class="graphDiv">
 			<canvas id="barChart"></canvas>
--- admin/js/graph-lite-admin.js
+++ admin/js/graph-lite-admin.js
@@ -1,23 +1,40 @@
 new Vue({
 	el: '#app',
 	data: {
-		chartType: 'pie'
+		chartType: 'pie',
+		chartlabelString: '',
+		chartDatasetDataString: '',
+		chartDatasetBgColorString: '',
+		labels: [],
+		DatasetData: [],
+		DatasetBgColor: []
 	},
 	methods: {
+		addLabels() {
+			let separatingLabels = this.labels = this.chartlabelString.split(',');
+			this.theChart.data.labels = separatingLabels;
+			this.theChart.update();
+		},
+		addDatasetData() {
+			let separatingDatasetData = this.DatasetData = this.chartDatasetDataString.split(',');
+			this.theChart.data.datasets[0].data = separatingDatasetData;
+			this.theChart.update();
+		},
+		addDatasetBgColor() {
+			let separatingDatasetColor = this.DatasetBgColor = this.chartDatasetBgColorString.split(',');
+			this.theChart.data.datasets[0].backgroundColor = separatingDatasetColor;
+			this.theChart.update();
+		},
 		onLoad() {
 			var ctx = document.getElementById("barChart");
-			var myChart = new Chart(ctx, {
+			this.theChart = new Chart(ctx, {
 				type: this.chartType,
 				data: {
-					labels: ['Red', 'Yellow', 'Blue'],
+					labels: [],
 					datasets: [
 						{
-							data: [10, 20, 30],
-							backgroundColor: [
-								'#FF0000',
-								'#FFFF00',
-								'#0000FF'
-							]
+							data: [],
+							backgroundColor: []
 						}
 					]
 				},
