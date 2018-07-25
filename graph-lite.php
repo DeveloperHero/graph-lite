ec428548db12e48c648edd19acbb610a9a4645e2
M Yakub Mizan <ym@orchestra.ltd>
2018-07-25 16:10:12 +0600

    Revert "changed file names to match with plugin name"
    This reverts commit db3876539438bd53035e6b83e7330c442fdef2e0.

--- graph-lite.php
+++ graph-lite.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Minimalist, user friendly graph and charting plugin for Wordpress 
+ * The plugin bootstrap file
  *
  * This file is read by WordPress to generate the plugin information in the plugin
  * admin area. This file also includes all of the dependencies used by the plugin,
@@ -10,10 +10,10 @@
  *
  * @link              http://orchestra.ltd
  * @since             1.0.0
- * @package           Graphs_Lite
+ * @package           Graph_Lite
  *
  * @wordpress-plugin
- * Plugin Name:       Graphs Lite
+ * Plugin Name:       Graph Lite
  * Plugin URI:        http://wordpress.org/plugins/graph-lite
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
@@ -21,7 +21,7 @@
  * Author URI:        http://orchestra.ltd
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       graphs-lite
+ * Text Domain:       graph-lite
  * Domain Path:       /languages
  */
 
