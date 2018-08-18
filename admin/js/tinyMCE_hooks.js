77a1d32aad28598162aac8e58055ee982948b2ce
Mishuk <mishuk.pq.bd@gmail.com>
2018-08-19 02:26:07 +0600

    Temporary tinny icon removed

--- admin/js/tinyMCE_hooks.js
+++ admin/js/tinyMCE_hooks.js
@@ -2,8 +2,9 @@
 	tinymce.PluginManager.add('graphs_lite_mce_btn', function( editor, url ) {
 		editor.addButton( 'graphs_lite_mce_btn', {
 			title: 'Insert Graph',
-			icon: true,
-			image: gl.admin_dir_url.'/images/16x16.png',
+			// icon: true,
+			text: 'Graph',
+			// image: gl.admin_dir_url+'images/16x16.png',
 			type: 'button',
 			onclick: function() {
 				$('#gl-admin-meta-box').fadeIn();
